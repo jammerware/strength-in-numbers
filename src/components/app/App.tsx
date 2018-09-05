76b7ab40275444e34e639e6002ab35180dd9b498
ben <ben@jammerware.com>
2018-09-05 14:41:03 -0400

    Initial addition of material-ui

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,27 +1,75 @@
 import * as React from 'react';
 import { Route, Link } from 'react-router-dom';
+
+import { createStyles, withStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
 import { LandingComponent } from '../landing/component-landing';
 import { RoomComponent } from '../room/component-room';
 import { RoomsComponent } from '../rooms/component-rooms';
 import { LoginComponent } from '../login/component-login';
 import './App.css';
-import logo from './logo.svg';
 
-class App extends React.Component {
+const styles = (theme: Theme) => createStyles({
+    root: {
+        flexGrow: 1,
+        height: 440,
+        zIndex: 1,
+        overflow: 'hidden',
+        position: 'relative',
+        display: 'flex',
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+    },
+    drawerPaper: {
+        position: 'relative',
+        width: 240,
+    },
+    content: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.default,
+        padding: theme.spacing.unit * 3,
+        minWidth: 0, // So the Typography noWrap works
+    },
+    toolbar: theme.mixins.toolbar,
+});
+
+class App extends React.Component<{ classes: any }, {}> {
     public render() {
         return (
             <div className="app">
-                <header className="app-header">
-                    <img src={logo} className="app-logo" alt="logo" />
-                    <h1 className="app-title">Strength in Numbers</h1>
-                </header>
+                <AppBar position="static" className={this.props.classes.appBar}>
+                    <Toolbar>
+                        <Typography variant="title" color="inherit" noWrap>
+                            Strength in Numbers
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
 
-                <nav>
-                    <Link to="/">Home</Link>
-                    <Link to="/rooms">Rooms</Link>
-                    <Link to="/rooms/1234">Video tech demo</Link>
-                    <Link to="/login">Log in</Link>
-                </nav>
+                <Drawer variant="permanent">
+                    <Divider />
+                    <List component="nav">
+                        <ListItem button>
+                            <ListItemText primary="Trash" />
+                        </ListItem>
+                        <ListItem button>
+                            <ListItemText primary="Things" />
+                        </ListItem>
+                    </List>
+                </Drawer>
+
+                <Link to="/">Home</Link>
+                <Link to="/rooms">Rooms</Link>
+                <Link to="/rooms/1234">Video tech demo</Link>
+                <Link to="/login">Log in</Link>
                 <div className="content-container">
                     <Route path="/rooms" exact component={RoomsComponent} />
                     <Route path="/rooms/:roomId" component={RoomComponent} />
@@ -33,4 +81,4 @@ class App extends React.Component {
     }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import App from './components/app/App';
 import './index.css';
 import { AuthProvider } from './providers/provider-auth';
@@ -9,10 +10,15 @@ import registerServiceWorker from './registerServiceWorker';
 // initialize firebase
 new AuthProvider().initFirebaseApp();
 
+// create MUI theme so it's available in child components
+const theme = createMuiTheme();
+
 // render the app
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <MuiThemeProvider theme={theme}>
+            <App />
+        </MuiThemeProvider>
     </BrowserRouter>,
     document.getElementById('root') as HTMLElement
 );
